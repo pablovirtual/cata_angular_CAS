0807263adde9b12b51a1bed12159fba855e8de8c
pablovirtual <143946760+pablovirtual@users.noreply.github.com>
2025-03-30 11:54:13 -0600

    boton

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,12 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
 import { NavbarComponent } from '../../shared/navbar/navbar.component';
 import { SafePipe } from '../../pipes/safe.pipe';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule, NavbarComponent, SafePipe],
+  imports: [CommonModule, NavbarComponent, SafePipe, RouterLink],
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
